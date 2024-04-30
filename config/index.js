872cd58278e622d91736a2c4cbcee444b82d2ec5
yangYangxs <48903673+yangYangxs@users.noreply.github.com>
2024-04-30 18:24:05 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wxd3f11bcb1cdbf4c8",
+  appsecret: "126a65dc52d7f79ba2712c5bf0a1da4d",
+  template_id: "bsemY1Er5Vvz_0zA3EY5aC4qj6fbrbETJR48F0Vj-8o",
+  touser: "o4rl91X2i4oeyvxxqfpV4HgqSvUc",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "成都",
+  first_date: "2023-08-26",
 };
